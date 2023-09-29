9ef00388ec4f3da2e5208acd3fddf72f67456bbd
strangelf47829 <strangelf47829@gmail.com>
2023-09-29 12:52:13 +0200

    Licensing info added

--- samples/src/Debugger/GraphicsDriver.cpp
+++ samples/src/Debugger/GraphicsDriver.cpp
@@ -1,3 +1,23 @@
+/*
+
+Copyright 2023 PCiD
+
+This file is part of BBP.
+
+BBP is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser General
+Public License as published by the Free Software Foundation, either version 3 of the License, or (at your
+option) any later version.
+
+BBP is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even
+the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License along with BBP. If not, see
+<https://www.gnu.org/licenses/>.
+
+Author: Rafael de Bie
+
+*/
 
 #include "../include/kernel.h"
 
